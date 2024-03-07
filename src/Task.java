132381a2d1e499e62d65ef6dbfff2aba1963c47f
Kroger-1 <148895862+Kroger-1@users.noreply.github.com>
2024-03-07 14:23:59 -0500

    Update Task.java

--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    private String taskTitle;
+    private String taskDescription;
+    private int taskPriority;
+
+    public Task(String taskTitle, String taskDescription, int taskPriority) {
+        this.taskTitle = taskTitle;
+        this.taskDescription = taskDescription;
+        this.taskPriority = taskPriority;
+    }
+
+    public String getTaskTitle() {
+        return taskTitle;
+    }
+
+    public void setTaskTitle(String taskTitle) {
+        this.taskTitle = taskTitle;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public int getTaskPriority() {
+        return taskPriority;
+    }
+
+    public void setTaskPriority(int taskPriority) {
+        this.taskPriority = taskPriority;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "taskTitle='" + taskTitle + '\'' +
+                ", taskDescription='" + taskDescription + '\'' +
+                ", taskPriority=" + taskPriority +
+                '}';
+    }
 }
